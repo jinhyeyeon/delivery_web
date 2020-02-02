315deaa963eda272ff4d3aefacac5f58a4a6a78a
혜연 <jhy199@naver.com>
2020-02-03 01:41:05 +0900

    :construction: 중간저장 #issue-3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles/common.scss';
+import Header from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      test
+      <Header />
     </div>
   );
 }
